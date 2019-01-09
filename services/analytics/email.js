a68d6a4de3f15c5a070082dac18159b07ecb2dc9
Adam Bergman <adam@fransvilhelm.com>
2019-01-09 13:23:32 +0100

    fix: Rename context to data

--- services/analytics/email.js
+++ services/analytics/email.js
@@ -7,14 +7,14 @@ import * as helpers from '../../utils/handlebars-helpers';
 
 const readFile = promisify(fs.readFile);
 
-async function construct(context) {
+async function construct(data) {
   const templateSource = await readFile(
     path.join(__dirname, '../../static/email-templates/analytics.mjml'),
     'utf-8',
   );
 
   const hbsTemplate = Handlebars.compile(templateSource, { strict: true });
-  const mjmlTemplate = hbsTemplate(context, { helpers });
+  const mjmlTemplate = hbsTemplate(data, { helpers });
   const { html } = mjml(mjmlTemplate, {
     minify: true,
     validationLevel: 'strict',
--- services/analytics/index.js
+++ services/analytics/index.js
@@ -1,7 +1,8 @@
 import * as email from './email';
 
 async function send(event, context, callback) {
-  const html = await email.construct(context);
+  const data = {};
+  const html = await email.construct(data);
   callback(null, html);
 }
 
