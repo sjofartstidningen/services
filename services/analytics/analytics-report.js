1b95ae92d0153511704c7cd0db1a9b40a43b4edd
Adam Bergman <adam@fransvilhelm.com>
2019-07-02 16:17:16 +0200

    fix: Try fixing report errors

--- services/analytics/analytics-report.js
+++ services/analytics/analytics-report.js
@@ -3,28 +3,23 @@ import { getEnv } from '../../utils/env';
 
 const scopes = 'https://www.googleapis.com/auth/analytics.readonly';
 
-const viewId = getEnv('GOOGLE_VIEW_ID');
-const jwt = new google.auth.JWT(
-  getEnv('GOOGLE_CLIENT_EMAIL'),
-  null,
-  getEnv('GOOGLE_PRIVATE_KEY'),
-  scopes,
-);
-
-const authorize = jwt.authorize();
+const GOOGLE_CLIENT_EMAIL = getEnv('GOOGLE_CLIENT_EMAIL');
+const GOOGLE_PRIVATE_KEY = getEnv('GOOGLE_PRIVATE_KEY');
+const GOOGLE_VIEW_ID = getEnv('GOOGLE_VIEW_ID');
 
 async function analyticsReport(reportConfig) {
-  await authorize;
+  const jwt = new google.auth.JWT(
+    GOOGLE_CLIENT_EMAIL,
+    null,
+    GOOGLE_PRIVATE_KEY,
+    scopes,
+  );
+  await jwt.authorize();
 
   const result = await google.analyticsreporting('v4').reports.batchGet({
     auth: jwt,
     requestBody: {
-      reportRequests: [
-        {
-          viewId,
-          ...reportConfig,
-        },
-      ],
+      reportRequests: [{ viewId: GOOGLE_VIEW_ID, ...reportConfig }],
     },
   });
 
